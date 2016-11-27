979f2fe9e975c379a9415e1ee1573c15978a61de
Ryan johnson <rj@Ryans-MacBook-Pro.local>
2016-11-27 13:09:38 -0800

    cards now created using javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,24 @@
 //console.log("JS file is connected to HTML! Woo!")
-var cardOne = 'King';
+/*var cardOne = 'King';
 var cardTwo = 'King';
 var cardThree = 'Queen';
-var cardFour = 'Queen';
+var cardFour = 'Queen';*/
 //console.log(cardOne == cardTwo);
 //console.log(cardThree == cardFour);
-if(cardTwo === cardFour) {
+
+var createCards = function(){
+	var gameBoard = document.getElementById('game-board')
+	for (var i = 0; i<=3; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card'
+		gameBoard.appendChild(newCard)
+	}
+}
+
+createCards ()
+
+/*if(cardTwo === cardFour) {
 	alert("You found a match!")
 } else {
 	alert("Sorry, try again.")
-}
+}*/
